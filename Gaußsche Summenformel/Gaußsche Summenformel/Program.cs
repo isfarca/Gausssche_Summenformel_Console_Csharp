9d8b41584c1262e49e0260bfca351348b34912c9
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 17:32:30 +0100

    Add project.

---
+++ Gaußsche Summenformel/Gaußsche Summenformel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaußsche_Summenformel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Eingabe;
+            int MinZahl;
+            int MaxZahl;
+            int n = 0;
+            int Ergebnis;
+
+            Console.Write("Minimum: ");
+            Eingabe = Console.ReadLine();
+            MinZahl = Convert.ToInt32(Eingabe);
+
+            Console.Write("Maximum: ");
+            Eingabe = Console.ReadLine();
+            MaxZahl = Convert.ToInt32(Eingabe);
+
+            for (int i = MinZahl; i <= MaxZahl; i++)
+                n++;
+
+            Ergebnis = n * (n + 1) / 2;
+
+            Console.WriteLine("\n {0}", Ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
